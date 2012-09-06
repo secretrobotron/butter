22abc3d3d8c6b58fec653288980600a845754e35
secretrobotron <secretrobotron@gmail.com>
2012-09-06 14:13:52 -0400

    [t2217] exit strategy for migrate.js

--- cornfield/migrate.js
+++ cornfield/migrate.js
@@ -24,20 +24,76 @@ User = new Schema({
 }),
 UserModel = mongoose.model( 'User', User );
 
-function migrateData() {
+function migrateUserProjects(user, finishedCallback) {
+  var projectsToMigrate = user.projects.length;
+
+  function checkForDoneState() {
+    if ( projectsToMigrate === 0 ) {
+      finishedCallback();
+    }
+  }
+
+  console.log("Iterating over projects for " + user.email + " (" + user.projects.length + ")" );
+  user.projects.forEach( function( project ) {
+
+    console.log( project._id );
+    var newProject = new Project2Model({
+      data: project.data,
+      email: user.email,
+      name: project.name,
+      template: project.template
+    });
+
+    newProject.save( function( err ) {
+      if ( err ) {
+        --projectsToMigrate;
+        checkForDoneState();
+        console.log( err );
+        return;
+      }
+
+      project.remove();
+      user.save( function( err ) {
+        --projectsToMigrate;
+        checkForDoneState();
+        if ( err ) {
+          console.log( err );
+          return;
+        }
+      });
+    });
+  });
+}
+
+function migrateData(finishedCallback) {
   console.log("Attempting data migration");
+
+  var usersToMigrate;
+
+  function checkForDoneState() {
+    if ( usersToMigrate === 0 ) {
+      finishedCallback();
+    }
+  }
 
   UserModel.find( {}, function( err, users ) {
 
-    if ( users.length === 0 ) {
+    usersToMigrate = users.length;
+
+    if ( usersToMigrate === 0 ) {
       console.log("No users to migrate");
+      finishedCallback();
       return;
     }
 
     users.forEach( function( user ) {
+
       if ( user.projects.length == 0 ) {
         user.remove();
         user.save( function( err ) {
+          --usersToMigrate;
+          checkForDoneState();
+
           if ( err ) {
             console.log( err );
             return;
@@ -47,31 +103,9 @@ function migrateData() {
         });
       }
 
-      console.log("Iterating over projects for " + user.email + " (" + user.projects.length + ")" );
-      user.projects.forEach( function( project ) {
-
-        console.log( project._id );
-        var newProject = new Project2Model({
-          data: project.data,
-          email: user.email,
-          name: project.name,
-          template: project.template
-        });
-
-        newProject.save( function( err ) {
-          if ( err ) {
-            console.log( err );
-            return;
-          }
-
-          project.remove();
-          user.save( function( err ) {
-            if ( err ) {
-              console.log( err );
-              return;
-            }
-          });
-        });
+      migrateUserProjects(user, function(){
+        --usersToMigrate;
+        checkForDoneState();
       });
     });
   });
@@ -81,6 +115,8 @@ function migrateData() {
 mongoose.connect( 'mongodb://localhost/test', function( err ) {
   if ( !err ) {
     dbOnline = true;
-    migrateData();
+    migrateData(function(){
+      process.exit();
+    });
   }
 });
