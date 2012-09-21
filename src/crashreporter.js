9a71537a3eabfb5143a21e30b8e0068f980b4198
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-09-21 17:22:28 -0400

    WIP

---
+++ src/crashreporter.js
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the MIT license
+ * If a copy of the MIT license was not distributed with this file, you can
+ * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
+
+/**
+ * Module: CrashReporter
+ *
+ * Provides backend and UI for the crash reporter
+ */
+define( [ "dialog/dialog" ], function( Dialog ){
+
+  // Cache existing window.onerror
+  var _onerror = window.onerror ? window.onerror : function(){ return true; },
+      _butter;
+
+  return function( butter ) {
+
+    window.onerror = function( message, url, lineno ) {
+      if( !window.XHLHttpRequest ) {
+        return _onerror();
+      }
+
+      console.log(message, url, lineno, navigator.userAgent);
+
+      var dialog = Dialog.span( "crash" );
+      dialog.open();
+
+      return _onerror();
+    };
+
+  };
+
+});
---
+++ src/dialog/dialogs/crash.js
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the MIT license
+ *  * If a copy of the MIT license was not distributed with this file, you can
+ *  * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
+
+define([ "text!dialog/dialogs/crash.html", "dialog/dialog" ],
+  function( LAYOUT_SRC, Dialog ){
+    Dialog.register( "crash", LAYOUT_SRC, function ( dialog ) {
+      var rootElement = dialog.rootElement,
+          updateBtn = rootElement.querySelector( ".update" ),
+          infoBtn = rootElement.querySelector( ".icon-info-sign" ),
+          dialogInfo = rootElement.querySelector( ".dialog-info" );
+    });
+});
